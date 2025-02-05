d9b8e14606ee950679b5d7f757f78baa852ce0ce
Abdelmuttalib <hagar9886@gmail.com>
2025-02-05 18:48:03 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,165 @@
 export default function Home() {
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8"></main>
+      <main className="grid grid-cols-6 gap-8 py-20 px-44">
+        <div className="flex flex-col gap-8">
+          <div className="bg-tw-background w-60 h-20 rounded">Background</div>
+          <div className="bg-tw-background-inactive w-60 h-20 rounded">
+            Background Inactive
+          </div>
+          <div className="bg-tw-background-subtle w-60 h-20 rounded">
+            Background Subtle
+          </div>
+          <div className="bg-tw-background-soft w-60 h-20 rounded">
+            Background Soft
+          </div>
+          <div className="bg-tw-background-strong w-60 h-20 rounded">
+            Background Strong
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <div className="text-2xl text-tw-foreground w-60 h-20 rounded">
+            Foreground
+          </div>
+          <div className="text-2xl text-tw-foreground-inactive w-60 h-20 rounded">
+            Foreground Inactive
+          </div>
+          <div className="text-2xl text-tw-foreground-subtle w-60 h-20 rounded">
+            Foreground Subtle
+          </div>
+          <div className="text-2xl text-tw-foreground-soft w-60 h-20 rounded">
+            Foreground Soft
+          </div>
+          <div className="text-2xl text-tw-foreground-strong w-60 h-20 rounded">
+            Foreground Strong
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <input
+            className="border border-tw-border w-60 h-20 rounded px-3 placeholder:text-tw-foreground-subtle outline-none focus:outline-none focus-visible:outline-none focus:border-tw-primary focus:ring focus:ring-tw-primary-subtle"
+            type="text"
+            placeholder="Text"
+          />
+
+          <div className="border border-tw-border w-60 h-20 rounded"></div>
+          <div className="border border-tw-border-inactive w-60 h-20 rounded"></div>
+          <div className="border border-tw-border-subtle w-60 h-20 rounded"></div>
+          <div className="border border-tw-border-soft w-60 h-20 rounded"></div>
+          <div className="border border-tw-border-strong w-60 h-20 rounded"></div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <div className="bg-tw-icon-bg w-60 h-20 rounded">Icon</div>
+          <div className="bg-tw-icon-bg-inactive w-60 h-20 rounded">
+            Icon Inactive
+          </div>
+          <div className="bg-tw-icon-bg-subtle w-60 h-20 rounded">
+            Icon Subtle
+          </div>
+          <div className="bg-tw-icon-bg-soft w-60 h-20 rounded">Icon Soft</div>
+          <div className="bg-tw-icon-bg-strong w-60 h-20 rounded">
+            Icon Strong
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <div className="bg-tw-error w-60 h-20 rounded">Background</div>
+
+          <div className="bg-tw-error-subtle w-60 h-20 rounded">
+            Background Subtle
+          </div>
+          <div className="bg-tw-error-soft w-60 h-20 rounded">
+            Background Soft
+          </div>
+          <div className="bg-tw-error-strong w-60 h-20 rounded">
+            Background Strong
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <div className="bg-tw-warning w-60 h-20 rounded">Background</div>
+
+          <div className="bg-tw-warning-subtle w-60 h-20 rounded">
+            Background Subtle
+          </div>
+          <div className="bg-tw-warning-soft w-60 h-20 rounded">
+            Background Soft
+          </div>
+          <div className="bg-tw-warning-strong w-60 h-20 rounded">
+            Background Strong
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <div className="bg-tw-warning-subtle px-2 py-1 text-sm rounded-lg border border-tw-warning-soft text-tw-warning font-medium w-fit dark:bg-tw-warning-strong dark:border-transparent">
+            Subtle
+          </div>
+          <div className="bg-tw-warning-soft px-2 py-1 text-sm rounded-lg border border-tw-warning-soft text-tw-warning font-medium w-fit dark:bg-tw-warning-strong dark:border-transparent">
+            Soft
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <div className="bg-tw-info w-60 h-20 rounded">Background</div>
+
+          <div className="bg-tw-info-subtle w-60 h-20 rounded">
+            Background Subtle
+          </div>
+          <div className="bg-tw-info-soft w-60 h-20 rounded">
+            Background Soft
+          </div>
+          <div className="bg-tw-info-strong w-60 h-20 rounded">
+            Background Strong
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
+
+// colors: {
+//   tw: {
+//     background: "var(--background)",
+//     "background-inactive": "var(--background-inactive)",
+//     "background-subtle": "var(--background-subtle)",
+//     "background-soft": "var(--background-soft)",
+//     "background-strong": "var(--background-strong)",
+//     border: "var(--border)",
+//     "border-inactive": "var(--border-inactive)",
+//     "border-subtle": "var(--border-subtle)",
+//     "border-soft": "var(--border-soft)",
+//     "border-strong": "var(--border-strong)",
+//     foreground: "var(--foreground)",
+//     "foreground-inactive": "var(--foreground-inactive)",
+//     "foreground-subtle": "var(--foreground-subtle)",
+//     "foreground-soft": "var(--foreground-soft)",
+//     "icon-bg": "var(--icon-bg)",
+//     "icon-bg-inactive": "var(--icon-bg-inactive)",
+//     "icon-bg-subtle": "var(--icon-bg-subtle)",
+//     "icon-bg-soft": "var(--icon-bg-soft)",
+//     "icon-bg-strong": "var(--icon-bg-strong)",
+//     error: "var(--error)",
+//     "error-subtle": "var(--error-subtle)",
+//     "error-soft": "var(--error-soft)",
+//     "error-strong": "var(--error-strong)",
+//     success: "var(--success)",
+//     "success-subtle": "var(--success-subtle)",
+//     "success-soft": "var(--success-soft)",
+//     "success-strong": "var(--success-strong)",
+//     warning: "var(--warning)",
+//     "warning-subtle": "var(--warning-subtle)",
+//     "warning-soft": "var(--warning-soft)",
+//     "warning-strong": "var(--warning-strong)",
+//     info: "var(--info)",
+//     "info-subtle": "var(--info-subtle)",
+//     "info-soft": "var(--info-soft)",
+//     "info-strong": "var(--info-strong)",
+//     primary: "var(--primary)",
+//     "primary-inactive": "var(--primary-inactive)",
+//     "primary-subtle": "var(--primary-subtle)",
+//     "primary-dark": "var(--primary-dark)",
+//     "primary-darker": "var(--primary-darker)",
+//   },
+// }
--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -28,78 +28,6 @@ interface TypographyProps
 const typographyVariants = cva("font-normal leading-normal text-foreground", {
   variants: {
     variant: {
-      // // xs
-      // "xs/regular": "text-xs leading-xs",
-      // "xs/medium": "text-xs font-medium leading-xs",
-      // "xs/semibold": "text-xs font-semibold leading-xs",
-      // "xs/bold": "text-xs font-bold leading-xs",
-
-      // // sm
-      // "sm/regular": "text-sm leading-sm",
-      // "sm/medium": "text-sm font-medium leading-sm",
-      // "sm/semibold": "text-sm font-semibold leading-sm",
-      // "sm/bold": "text-sm font-bold leading-sm",
-
-      // // base
-      // "base/regular": "text-md leading-base",
-      // "base/medium": "text-md font-medium leading-base",
-      // "base/semibold": "text-md font-semibold leading-base",
-      // "base/bold": "text-md font-bold leading-base",
-
-      // // md
-      // "md/regular": "text-md leading-md",
-      // "md/medium": "text-md font-medium leading-md",
-      // "md/semibold": "text-md font-semibold leading-md",
-      // "md/bold": "text-md font-bold leading-md",
-
-      // // lg
-      // "lg/regular": "text-lg leading-lg",
-      // "lg/medium": "text-lg font-medium leading-lg",
-      // "lg/semibold": "text-lg font-semibold leading-lg",
-      // "lg/bold": "text-lg font-bold leading-lg",
-
-      // // xl
-      // "xl/regular": "text-xl leading-xl",
-      // "xl/medium": "text-xl font-medium leading-xl",
-      // "xl/semibold": "text-xl font-semibold leading-xl",
-      // "xl/bold": "text-xl font-bold leading-xl",
-
-      // // display-xs
-      // "display-xs/regular": "text-2xl leading-2xl",
-      // "display-xs/medium": "text-2xl font-medium leading-2xl",
-      // "display-xs/semibold": "text-2xl font-semibold leading-2xl",
-      // "display-xs/bold": "text-2xl font-bold leading-2xl",
-
-      // // display-sm
-      // "display-sm/regular": "text-3xl leading-3xl",
-      // "display-sm/medium": "text-3xl font-medium leading-3xl",
-      // "display-sm/semibold": "text-3xl font-semibold leading-3xl",
-      // "display-sm/bold": "text-3xl font-bold leading-3xl",
-
-      // // display-md
-      // "display-md/regular": "text-4xl leading-4xl",
-      // "display-md/medium": "text-4xl font-medium leading-4xl",
-      // "display-md/semibold": "text-4xl font-semibold leading-4xl",
-      // "display-md/bold": "text-4xl font-bold leading-4xl",
-
-      // // display-lg
-      // "display-lg/regular": "text-5xl leading-5xl",
-      // "display-lg/medium": "text-5xl font-medium leading-5xl",
-      // "display-lg/semibold": "text-5xl font-semibold leading-5xl",
-      // "display-lg/bold": "text-5xl font-bold leading-5xl",
-
-      // // display-xl
-      // "display-xl/regular": "text-6xl leading-6xl",
-      // "display-xl/medium": "text-6xl font-medium leading-6xl",
-      // "display-xl/semibold": "text-6xl font-semibold leading-6xl",
-      // "display-xl/bold": "text-6xl font-bold leading-6xl",
-
-      // // display-2xl
-      // "display-2xl/regular": "text-7xl leading-7xl",
-      // "display-2xl/medium": "text-7xl font-medium leading-7xl",
-      // "display-2xl/semibold": "text-7xl font-semibold leading-7xl",
-      // "display-2xl/bold": "text-7xl font-bold leading-7xl",
-
       // xs
       "xs/regular": "text-xs leading-xs font-regular",
       "xs/medium": "text-xs leading-xs font-medium",
@@ -227,17 +155,3 @@ function Typography({
 }
 
 export { Typography, type TypographyProps, typographyVariants };
-
-// type LineHeight =
-//   | 'xs'
-//   | 'sm'
-//   | 'base'
-//   | 'md'
-//   | 'lg'
-//   | 'xl'
-//   | 'display-xs'
-//   | 'display-sm'
-//   | 'display-md'
-//   | 'display-lg'
-//   | 'display-xl'
-//   | 'display-2xl';
